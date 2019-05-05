271605e32f5e962888eb7939728e466e7f6c4987
abdellah bouskine <abdearec@gmail.com>
2019-05-05 12:46:28 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const hbs = require("hbs");
+app.set("view engine", "hbs");
+
+//---------------- [ ========> middleware <======== ] ---------------\\
+
+// middleware function
+app.use((req, res, next) => {
+  var now = new Date();
+  var log = `${new Date()} : ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("Server.Log", log + "\n", err => {
+    if (err) {
+      console.log("Probleme d'ecrit sur le fichier");
+    }
+  });
+  next();
+});
+
+// this function whene the site in maintenace
+app.use((req, res, next) => {
+  res.render("maintenance.hbs");
+});
+
+//---------------- use all url after middleware ----------------\\
+
+// declare folder public as static folder
+app.use(express.static(__dirname + "/public"));
+
+//declare folder layers as partials folder
+hbs.registerPartials(__dirname + "/views/layers");
+
+// declare all helpers like getter and setter
+hbs.registerHelper("gethalpper01", () => {
+  return new Date();
+});
+
+app.get("/", (req, res) => {
+  //res.send("<h1>Abdellah Bouskine</h1>");
+  res.render("home.hbs", {
+    title: "Home Page",
+    name: "Home"
+    // date: new Date()
+  });
+});
+app.get("/about", (req, res) => {
+  //res.send("<h1>Abdellah Bouskine</h1>");
+  res.render("about.hbs", {
+    title: "About Page",
+    name: "About"
+    // date: new Date()
+  });
+});
+
+app.get("/name", (req, res) => {
+  res.send("<h1>abdellah Bouskine</h1>");
+});
+
+app.listen(8100, () => {
+  console.log("you are connecte in server (port:8100)");
+});
