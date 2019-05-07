cff3f7971cbc4d9240f5d94645335b0af725d515
abdellah bouskine <abdearec@gmail.com>
2019-05-07 09:40:53 +0000

    i dont know what i added

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,11 @@
 module.exports.add = (a, b) => {
   return a + b;
 };
+module.exports.SetName = (User, FullName) => {
+  var name = FullName.split(" ");
+  User.firstname = name[0];
+  User.lastname = name[1];
+  return User;
+};
 
 module.exports.square = x => x * x;
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,13 +1,32 @@
+const expect = require("expect");
 const utils = require("./utils");
+
+//test if have a two numbers
 it("should add two numbers", () => {
   var res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error("value not correct");
-  }
+  expect(res)
+    .toBe(44)
+    .toBeA("number");
+  // if (res !== 44) {
+  //   throw new Error("value not correct");
+  // }
 });
 it("should square a number", () => {
   var res = utils.square(3);
-  if (res !== 9) {
-    throw new Error("expect 9 but value isnt correct");
-  }
+  expect(res)
+    .toBe(9)
+    .toBeA("number");
+  // if (res !== 9) {
+  //   throw new Error("expect 9 but value isnt correct");
+  // }
+});
+
+it("should set FirstName and LastName", () => {
+  var user = { location: "kenitra", age: 25 };
+  var res = utils.SetName(user, "Abdellah Bouskine");
+  //expect(res).toEqual(user);
+  expect(res).toInclude({
+    firstname: "Abdellah",
+    lastname: "Bouskine"
+  });
 });
